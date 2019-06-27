fcd2d1a548caf89d45752c9d1ff5114fc9372ddf
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2019-06-27 12:14:48 +0200

    First commit of basic infrastructure for VKGL file processor.

---
+++ process_VKGL_data.php
@@ -0,0 +1,228 @@
+<?php
+/*******************************************************************************
+ *
+ * LEIDEN OPEN VARIATION DATABASE (LOVD)
+ *
+ * Created     : 2019-06-27
+ * Modified    : 2019-06-27
+ * Version     : 0.0
+ * For LOVD+   : 3.0-22
+ *
+ * Purpose     : Processes the VKGL consensus data, and creates or updates the
+ *               VKGL data in the LOVD instance.
+ *
+ * Changelog   : 0.1    2019-06-??
+ *               Initial release.
+ *
+ * Copyright   : 2004-2019 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *
+ * This file is part of LOVD.
+ *
+ * LOVD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LOVD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LOVD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *************/
+
+// Command line only.
+if (isset($_SERVER['HTTP_HOST'])) {
+    die('Please run this script through the command line.' . "\n");
+}
+
+// Default settings. Everything in 'user' will be verified with the user, and stored in settings.json.
+$_CONFIG = array(
+    'version' => '0.0',
+    'settings_file' => 'settings.json',
+    'columns_mandatory' => array(
+        // These are the columns that need to be present in order for the file to get processed.
+        'id',
+        'chromosome',
+        'start',
+        'ref',
+        'alt',
+        'gene',
+        'c_dna',
+        'transcript',
+        'protein',
+    ),
+    'columns_ignore' => array(
+        // These are the columns that we'll ignore. If we find any others, we'll complain.
+        'consensus_classification',
+        'matches',
+        'disease',
+        'comments',
+        'history',
+    ),
+    'columns_center_suffix' => '_link', // This is how we recognize a center, because it also has a *_link column.
+    'user' => array(
+        // Variables we will be asking the user.
+        'lovd_path' => '/www/databases.lovd.nl/shared/',
+    ),
+);
+
+// Exit codes.
+// See http://tldp.org/LDP/abs/html/exitcodes.html for recommendations, in particular:
+// "[I propose] restricting user-defined exit codes to the range 64 - 113 (...), to conform with the C/C++ standard."
+define('EXIT_OK', 0);
+define('EXIT_WARNINGS_OCCURRED', 64);
+define('EXIT_ERROR_INSUFFICIENT_ARGS', 65);
+define('EXIT_ERROR_INPUT_NOT_A_FILE', 66);
+define('EXIT_ERROR_INPUT_UNREADABLE', 67);
+define('EXIT_ERROR_INPUT_CANT_OPEN', 68);
+define('EXIT_ERROR_SETTINGS_CANNOT_CREATE', 69);
+define('EXIT_ERROR_SETTINGS_UNREADABLE', 70);
+define('EXIT_ERROR_SETTINGS_CANNOT_UPDATE', 71);
+define('EXIT_ERROR_HEADER_FIELDS_NOT_FOUND', 72);
+define('EXIT_ERROR_HEADER_FIELDS_INCORRECT', 73);
+define('EXIT_ERROR_DATA_FIELD_COUNT_INCORRECT', 74);
+
+define('VERBOSITY_NONE', 0); // No output whatsoever.
+define('VERBOSITY_LOW', 3); // Low output, only the really important messages.
+define('VERBOSITY_MEDIUM', 5); // Medium output. No output if there is nothing to do. Useful for when using cron.
+define('VERBOSITY_HIGH', 7); // High output. The default.
+define('VERBOSITY_FULL', 9); // Full output, including debug statements.
+
+
+
+
+
+function lovd_printIfVerbose ($nVerbosity, $sMessage)
+{
+    // This function only prints the given message when the current verbosity is set to a level high enough.
+
+    // If no verbosity is currently defined, just print everything.
+    if (!defined('VERBOSITY')) {
+        define('VERBOSITY', 9);
+    }
+
+    if (VERBOSITY >= $nVerbosity) {
+        // Write to STDERR, as this script dumps the resulting output file to STDOUT.
+        fwrite(STDERR, $sMessage);
+    }
+    return true;
+}
+
+
+
+
+
+function lovd_verifySettings ($sKeyName, $sMessage, $sVerifyType, $options)
+{
+    // Based on a function provided by Ileos.nl in the interest of Open Source.
+    // Check if settings match certain input.
+    global $_CONFIG;
+
+    switch($sVerifyType) {
+        case 'array':
+            $aOptions = $options;
+            if (!is_array($aOptions)) {
+                return false;
+            }
+            break;
+
+        case 'int':
+            // Integer, options define a range in the format '1,3' (1 to 3) or '1,' (1 or higher).
+            $aRange = explode(',', $options);
+            if (!is_array($aRange) ||
+                ($aRange[0] === '' && $aRange[1] === '') ||
+                ($aRange[0] !== '' && !ctype_digit($aRange[0])) ||
+                ($aRange[1] !== '' && !ctype_digit($aRange[1]))) {
+                return false;
+            }
+            break;
+    }
+
+    while (true) {
+        print('  ' . $sMessage .
+            ($sVerifyType != 'int' || ($aRange === array('', ''))? '' : ' (' . (int) $aRange[0] . '-' . $aRange[1] . ')') .
+            (empty($_CONFIG['user'][$sKeyName])? '' : ' [' . $_CONFIG['user'][$sKeyName] . ']') . ' : ');
+        $sInput = trim(fgets(STDIN));
+        if (!strlen($sInput) && !empty($_CONFIG['user'][$sKeyName])) {
+            $sInput = $_CONFIG['user'][$sKeyName];
+        }
+
+        switch ($sVerifyType) {
+            case 'array':
+                $sInput = strtolower($sInput);
+                if (in_array($sInput, $aOptions)) {
+                    $_CONFIG['user'][$sKeyName] = $sInput;
+                    return true;
+                }
+                break;
+
+            case 'int':
+                $sInput = (int) $sInput;
+                // Check if input is lower than minimum required value (if configured).
+                if ($aRange[0] !== '' && $sInput < $aRange[0]) {
+                    break;
+                }
+                // Check if input is higher than maximum required value (if configured).
+                if ($aRange[1] !== '' && $sInput > $aRange[1]) {
+                    break;
+                }
+                $_CONFIG['user'][$sKeyName] = $sInput;
+                return true;
+
+            case 'string':
+                $_CONFIG['user'][$sKeyName] = $sInput;
+                return true;
+
+            case 'file':
+            case 'lovd_path':
+            case 'path':
+                // Always accept the default (if non-empty) or the given options.
+                if (($sInput && ($sInput == $_CONFIG['user'][$sKeyName] ||
+                        $sInput === $options)) ||
+                    (is_array($options) && in_array($sInput, $options))) {
+                    $_CONFIG['user'][$sKeyName] = $sInput; // In case an option was chosen that was not the default.
+                    return true;
+                }
+                if (in_array($sVerifyType, array('lovd_path', 'path')) && !is_dir($sInput)) {
+                    print('    Given path is not a directory.' . "\n");
+                    break;
+                } elseif (!is_readable($sInput)) {
+                    print('    Cannot read given path.' . "\n");
+                    break;
+                }
+
+                if ($sVerifyType == 'lovd_path') {
+                    if (!file_exists($sInput . '/config.ini.php')) {
+                        if (file_exists($sInput . '/src/config.ini.php')) {
+                            $sInput .= '/src';
+                        } else {
+                            print('    Cannot locate config.ini.php in given path.' . "\n" .
+                                  '    Please check that the given path is a correct path to an LOVD installation.' . "\n");
+                            break;
+                        }
+                    }
+                    if (!is_readable($sInput . '/config.ini.php')) {
+                        print('    Cannot read configuration file in given LOVD directory.' . "\n");
+                        break;
+                    }
+                    // We'll set everything up later, because we don't want to
+                    // keep the $_DB open for as long as the user is answering questions.
+                }
+                $_CONFIG['user'][$sKeyName] = $sInput;
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    return false; // We'd actually never get here.
+}
+
+
+?>
