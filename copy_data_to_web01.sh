f6de32a5c26f6381829924690c93906f1ec0f7ad
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2023-07-14 12:01:12 +0200

    Update the bash script that push the data to the remote servers.
    The new cache synching script takes over part of the work.
    Also, it's better to leave the user out now.

--- copy_data_to_web01.sh
+++ copy_data_to_web01.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-echo "This will overwrite the remote caches. Are you sure?";
-read;
-
-rsync -av $(ls -1 /www/git/VKGL_import/vkgl_consensus_$(date +%Y-%m)* | grep -v chr1 | tr '\n' ' ') ifokkema@web01:/home/ifokkema/git/VKGL_import/
-rsync -av /www/git/caches/NC_cache.txt /www/git/caches/mapping_cache.txt ifokkema@web01:/home/ifokkema/git/caches/
+rsync -av $(ls -1 /www/git/VKGL_import/$(date +%Y-%m)/vkgl_consensus_* | tr '\n' ' ') "web01:/home/${USER}/git/VKGL_import/"
 
