3ee1f07deeb80ae94642890775e724640fbead20
Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
2023-11-29 09:43:22 +0100

    generate_reports.sh should now be run from the data directory.
    It makes the code quite a lot easier.
    
    Also, fetch the month from the directory, so we can run the script
     even when we're already in the next month.

--- generate_reports.sh
+++ generate_reports.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
+DATE=$(pwd | rev | cut -d / -f 1 | rev);
+
 # Internal conflicts:
-cat stats.txt | grep -A 1000 \
-  $(grep vkgl_consensus_ stats.txt | tail -n 1 | cut -d '[' -f 2 | cut -d ']' -f 1) \
-  | grep internal \
-  | sed -r 's/^\s+//' > vkgl_internal_conflicts_$(date +%Y-%m).txt
-echo "Created vkgl_internal_conflicts_$(date +%Y-%m).txt.";
+grep internal formatting.log \
+  | sed -r 's/^\s+//' > "vkgl_internal_conflicts_${DATE}.log"
+echo "Created vkgl_internal_conflicts_${DATE}.log.";
 
 
 
 # Errors:
-LAST_FILE=$(ls -1t output_202*full-run* | head -n 1);
-LAST_MONTH=$(echo $LAST_FILE | cut -d _ -f 2 | cut -d . -f 1);
+LAST_FILE="output.01.debugging-to-build-caches.log";
 
-grep "{" $LAST_FILE | grep Error | tr '|' '\t' | cut -f 2- | sed 's/}$//' | sort -g > vkgl_errors_${LAST_MONTH}.txt
+grep "{" "${LAST_FILE}" | grep Error | tr '|' '\t' | cut -f 2- | sed 's/}$//' | sort -g > "vkgl_errors_${DATE}.log"
 
-for CHR in $(cat chromosomes.txt);
+for CHR in $(cat ../chromosomes.txt);
 do
-  grep "^${CHR}\s" vkgl_errors_${LAST_MONTH}.txt | sort -k 2 -g;
-done > vkgl_errors_${LAST_MONTH}.sorted.txt
+  grep "^${CHR}\s" vkgl_errors_${DATE}.log | sort -k 2 -g;
+done > vkgl_errors_${DATE}.sorted.log
 
-mv vkgl_errors_${LAST_MONTH}.sorted.txt vkgl_errors_${LAST_MONTH}.txt
-echo "Created vkgl_errors_${LAST_MONTH}.txt.";
+mv vkgl_errors_${DATE}.sorted.log vkgl_errors_${DATE}.log
+echo "Created vkgl_errors_${DATE}.log.";
 
 
 
 # Summary:
 echo "Summary of errors:";
-cut -f 7 vkgl_errors_${LAST_MONTH}.txt | grep -v '^$' | tr ',' '\n' | sort | uniq -c | sort -g
+cut -f 7 vkgl_errors_${DATE}.log | grep -v '^$' | tr ',' '\n' | sort | uniq -c | sort -g
 
 
 
 # Conflicts:
-grep "{" $LAST_FILE | grep ConflictHeader | tr '|' '\t' | cut -f 2- \
-  | sed 's/}$//' > vkgl_opposites_${LAST_MONTH}.txt
-grep "{" $LAST_FILE | grep Conflict | head -n -1 | sort -g | tr '|' '\t' \
-  | cut -f 2- | sed 's/}$//' >> vkgl_opposites_${LAST_MONTH}.txt
-echo "Created vkgl_opposites_${LAST_MONTH}.txt.";
+grep "{" "${LAST_FILE}" | grep ConflictHeader | tr '|' '\t' | cut -f 2- \
+  | sed 's/}$//' > vkgl_opposites_${DATE}.log
+grep "{" "${LAST_FILE}" | grep Conflict | head -n -1 | sort -g | tr '|' '\t' \
+  | cut -f 2- | sed 's/}$//' >> vkgl_opposites_${DATE}.log
+echo "Created vkgl_opposites_${DATE}.log.";
 
