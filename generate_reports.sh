1cadd205ebcd55aa8021f51f30cace568ffce77c
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2024-09-18 11:01:47 +0200

    Improve the reporting script; add more details.
    Those details are useful for the email to the VKGL, and this saves
     me from running stuff by myself.

--- generate_reports.sh
+++ generate_reports.sh
@@ -37,3 +37,21 @@ grep "{" "${LAST_FILE}" | grep Conflict | head -n -1 | sort -g | tr '|' '\t' \
   | cut -f 2- | sed 's/}$//' >> vkgl_opposites_${DATE}.log
 echo "Created vkgl_opposites_${DATE}.log.";
 
+
+
+# Generate statistics for the VKGL email.
+# All stats can be fetched from the last run.
+DATA=$(grep -EA4 "\[(100.0%|Totals)\]" output.03.full-run-with-deletes.log)
+echo "
+Unique variants received : $(echo "$DATA" | grep "VKGL file successfully parsed," | sed 's/^ *//' | cut -d \  -f 8) (after filtering out internal conflicts)
+Unique variants in error : $(echo "$DATA" | grep "Variants lost:" | sed 's/^ *//' | cut -d \  -f 3 | cut -d . -f 1)    -
+Unique variants merged   : $(echo "$DATA" | grep "variants merged. Variants left:" | sed 's/^ *//' | cut -d \  -f 3)  -
+                           ======
+Unique variants left     : $(echo "$DATA" | grep "variants merged. Variants left:" | sed 's/^ *//' | cut -d \  -f 8 | cut -d . -f 1)
+                           ======
+$(echo "$DATA" | grep -A 3 "Single-lab" | sed 's/^        //')
+=================================
+Total classifications    : $(echo "$DATA" | tail | grep " Variants " | grep -v "deleted" | cut -b 20- | cut -d : -f 2- | sed 's/\.$//' | paste -sd+ | bc)
+                           ======
+$(echo "$DATA" | tail | grep " Variants " | cut -b 20- | sed 's/\.$//' | sed 's/Variants/Classifications/' | sed 's/:/  :/')
+";
