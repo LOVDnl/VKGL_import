ccb733627784389824b5fd8aa6daa6e9c77143c3
Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
2026-02-23 15:50:02 +0100

    Add a helper class to write to log files.
    It can be configured to also write to the screen at the same time.

---
+++ log.php
@@ -0,0 +1,96 @@
+<?php
+/*******************************************************************************
+ *
+ * VKGL-LOVD data pipeline.
+ *
+ * Created     : 2026-02-23
+ * Modified    : 2026-02-23
+ *
+ * Copyright   : 2004-2026 Leiden University Medical Center; http://www.LUMC.nl/
+ * Programmer  : Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
+ *
+ *************/
+
+namespace LOVD;
+
+class Log
+{
+    // Class abstracting the use of a status.log file for pipelines.
+    // The actual filename can be set using the constructor.
+    private bool $printToScreen = false;
+    private string $file = __DIR__ . '/status.log';
+    private string $last_line = '';
+
+
+
+    public function __construct (string $sFile = null)
+    {
+        // If the file does not exist, create it.
+        if ($sFile) {
+            $this->file = $sFile;
+        }
+
+        if (file_exists($this->file)) {
+            if (!is_readable($this->file) || !is_writable($this->file) || is_dir($this->file)) {
+                throw new \Exception("Unable to load {$this->file}");
+            }
+
+            // Get the last line out. Strip the timestamp, if present.
+            $this->last_line = ltrim(strrchr(rtrim(file_get_contents($this->file)), "\n"));
+            if (preg_match('/^[0-9: -]{19} .. /', $this->last_line)) {
+                $this->last_line = substr($this->last_line, 23);
+            }
+
+        } elseif (!touch($this->file)) {
+            throw new \Exception("Unable to create {$this->file}");
+        }
+    }
+
+
+
+    public function add (string $sLog, string $sCode = ''): bool
+    {
+        $this->last_line = $sLog;
+        $sLog = date('Y-m-d H:i:s ') . str_pad($sCode, 3) . str_replace("\n", str_repeat(' ', 23) . "\n", rtrim($sLog)) . "\n";
+        if ($this->printToScreen) {
+            echo $sLog;
+        }
+        return (bool) file_put_contents(
+            $this->file,
+            $sLog,
+            FILE_APPEND
+        );
+    }
+
+
+
+    public function addBreak (): bool
+    {
+        return (bool) file_put_contents($this->file, "\n", FILE_APPEND);
+    }
+
+
+
+    public function addBreakIfNotEmpty (): bool
+    {
+        if ($this->last_line) {
+            return $this->addBreak();
+        }
+        return true;
+    }
+
+
+
+    public function getLastLine (): string
+    {
+        return $this->last_line;
+    }
+
+
+
+    public function printToScreen (bool $bPrint): bool
+    {
+        $this->printToScreen = $bPrint;
+        return $this->printToScreen;
+    }
+}
