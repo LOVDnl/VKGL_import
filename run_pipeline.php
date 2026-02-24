81e2c01c3af2f1511493cbb749e780e8a2d75f2d
Ivo F.A.C. Fokkema <I.F.A.C.Fokkema@LUMC.nl>
2026-02-24 18:09:17 +0100

    Check the server settings when available, and get passphrases.
    We'll fully automate interaction with the servers, so we'll need
     to have the SSH key passphrases. To make sure we can check them
     immediately when given, we should connect to the server, but
     that's overkill. So, instead, store the hash and compare. That
     doesn't guarantee that they will work, but once a working hash has
     been cached, we'll have a quick check.

--- run_pipeline.php
+++ run_pipeline.php
@@ -49,6 +49,45 @@ foreach([
 
 
 
+// Checking the server settings, and collecting passphrases when needed.
+$aPassphrases = [];
+foreach (($Settings->get('servers') ?? []) as $sServer => $aServer) {
+    if (!empty($aServer['key']) && !isset($aPassphrases[$aServer['key']])) {
+        $aPassphrases[$aServer['key']] = '';
+        // Check the key file and check if it's encrypted.
+        if (!file_exists($aServer['key']) || !is_readable($aServer['key'])) {
+            print("SSH key configured for server $sServer not found or not readable.\n");
+            die($Settings->get("error_codes|EXIT_ERROR_SETTINGS_PROBLEM"));
+        }
+        if (str_contains(file_get_contents($aServer['key']), 'ENCRYPTED')) {
+            // Key is encrypted. Ask for the passphrase.
+            while (true) {
+                print("SSH key {$aServer['key']} is encrypted, please provide the passphrase:\n");
+                // Disable echo, so the user can safely type the key without anyone seeing.
+                system('stty -echo');
+                $sInput = trim(fgets(STDIN));
+                // Re-enable echo, so the user can use their terminal again.
+                system('stty echo');
+
+                // If we have a hash stored, compare.
+                $sHash = password_hash($sInput, PASSWORD_DEFAULT);
+                if (empty($aServer['passphrase_hash'])) {
+                    $Settings->set("servers|{$sServer}|passphrase_hash", $sHash);
+                    print("SSH key passphrase hash cached for future use.\n");
+                    break;
+                } elseif (!password_verify($sInput, $aServer['passphrase_hash'])) {
+                    print("SSH key passphrase hash does not match — clear the cached hash or try again.\n");
+                } else {
+                    break;
+                }
+            }
+            $aPassphrases[$aServer['key']] = $sInput;
+        }
+    }
+}
+
+
+
 // First, determine which release we're supposed to be working on.
 $aMonths = $Settings->get('release_months');
 if ($aMonths === null) {
