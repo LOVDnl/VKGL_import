3e286a2f123438ba09d612fdfc411c1762ad2a98
Marit <m.de_koster@lumc.nl>
2026-07-07 10:17:41 +0200

    Adding function wich opens the connection to LOVD to processor.php.

--- processor.php
+++ processor.php
@@ -69,6 +69,38 @@ class Processor
     private $Settings;
 
     private $Log;
+
+    public function connectLOVD()
+    {
+        // These variables are global scope, this way lovd function can access them.
+        global $_CONF, $_DB, $_TABLES, $_SETT;
+        // Open connection, and check if user accounts exist.
+        $this->Log->add("Connecting to LOVD...");
+        // Find LOVD installation, run it's inc-init.php to get DB connection, initiate $_SETT, etc.
+        define('ROOT_PATH', $this->Settings->get('lovd_path') . '/');;
+        define('FORMAT_ALLOW_TEXTPLAIN', true);
+        $_GET['format'] = 'text/plain';
+        // To prevent notices when running inc-init.php.
+        $this->_SERVER = array_merge($this->_SERVER, array(
+            'HTTP_HOST' => 'localhost',
+            'REQUEST_URI' => '/' . basename(__FILE__),
+            'QUERY_STRING' => '',
+            'REQUEST_METHOD' => 'GET',
+        ));
+        // If I put a require here, I can't nicely handle errors, because PHP will die if something is wrong.
+        // However, I need to get rid of the "headers already sent" warnings from inc-init.php.
+        // So, sadly if there is a problem connecting to LOVD, the script will die here without any output whatsoever.
+        ini_set('display_errors', '0');
+        ini_set('log_errors', '0'); // CLI logs errors to the screen, apparently.
+        // Let the LOVD believe we're accessing it through SSL. LOVDs that demand this, will otherwise block us.
+        // We have error messages suppressed anyway, as the LOVD in question will complain when it tries to define "SSL" as well.
+        define('SSL', true);
+        require ROOT_PATH . 'inc-init.php';
+        require ROOT_PATH . 'inc-lib-form.php';
+        require ROOT_PATH . 'inc-lib-variants.php'; // For lovd_fixHGVS().
+        ini_set('display_errors', '1'); // We do want to see errors from here on.
+        $this->Log->add("Connected...");
+    }
     
 }
 ?>
