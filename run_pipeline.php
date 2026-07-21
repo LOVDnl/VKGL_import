2ace0b764d736fc42498f713b2233b746fdbd50d
Marit <m.de_koster@lumc.nl>
2026-07-21 11:35:16 +0200

    Check to see if the statistics file (created by the validator) exists on the remote server.

--- run_pipeline.php
+++ run_pipeline.php
@@ -466,14 +466,26 @@ if ($Status->get('step') < $nStep) {
 
     $aStatistics = $Statistics->get($sRelease.'|diff');
     $aProcessorStatistics = $o->getStatistics();
-    $aCommonKeys = array_intersect_key($aStatistics, $aProcessorStatistics);
-    // $aCommonKeys has the values of $aStatistics.
-    $nMisMatch = 0;
-    $sShowExpectations = "results: Expected Reality\n";
-    foreach ($aCommonKeys as $Key => $Value) {
-        $sShowExpectations .= $Key . ": ". $aCommonKeys[$Key] . "\t " . $aProcessorStatistics[$Key] . "\n";
-        if ($aCommonKeys[$Key] != $aProcessorStatistics[$Key]) {
-            $nMisMatch ++;
+    // Checking to see if the statistics of the validator step are on the remote server.
+    if ($aStatistics) {
+        $aCommonKeys = array_intersect_key($aStatistics, $aProcessorStatistics);
+        // $aCommonKeys has the values of $aStatistics.
+        $nMisMatch = 0;
+        $sShowExpectations = "results: Expected Reality\n";
+        foreach ($aCommonKeys as $Key => $Value) {
+            $sShowExpectations .= $Key . ": " . $aCommonKeys[$Key] . "\t " . $aProcessorStatistics[$Key] . "\n";
+            if ($aCommonKeys[$Key] != $aProcessorStatistics[$Key]) {
+                $nMisMatch++;
+            }
+        }
+        $Log->add($sShowExpectations);
+        try {
+            if ($nMisMatch > 0) {
+                throw new Exception("The statistics don't match");
+            }
+        } catch (Exception $e) {
+            $Log->add("Something went wrong.\n" . $e->getMessage() . '.', '!!');
+            die($Settings->get('error_codes|EXIT_WARNINGS_OCCURRED'));
         }
     }
     $Log->add($sShowExpectations);
