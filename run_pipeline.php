c52fcb3915ca26ccab1dea27a1122028d36b916e
Marit <m.de_koster@lumc.nl>
2026-08-04 13:54:27 +0200

    Removing $Log when calling validator.php, the validator doesn't use the log file.

--- run_pipeline.php
+++ run_pipeline.php
@@ -476,7 +476,7 @@ if ($Status->get('step') < $nStep) {
 
     // Now call the validator and pass on the files that need validation (i.e., their contents are compared).
     try {
-        $o = Validator::validate(PREVIOUS_RELEASE_PATH . '/' . $PreviousStatus->get('output_files|aggregated'), $Status->get('output_files|aggregated'), ($Settings->get('validation_cutoffs|aggregated') ?? []), $Log);
+        $o = Validator::validate(PREVIOUS_RELEASE_PATH . '/' . $PreviousStatus->get('output_files|aggregated'), $Status->get('output_files|aggregated'), ($Settings->get('validation_cutoffs|aggregated') ?? []));
     } catch (Exception $e) {
         $Log->add("Failed to validate the aggregated output.\n" . $e->getMessage() . '.', '!!');
         die($Settings->get('error_codes|EXIT_ERROR_INPUT_CONTENT_ERROR'));
